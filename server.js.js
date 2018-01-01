a555349d38f7b9a5b742cbc3ec15ea0730633348
VictorCastro <viracami2>
2017-12-31 19:18:04 -0500

    fix: 1.4 updates to deploy

--- server.js.js
+++ server.js.js
@@ -1,18 +1,8 @@
-//Install express server
-const express = require('express');
-const app = express();
-const path = require('path');
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+////////
+const express = require('express'); 
+const app = express(); 
+app.use(express.static(__dirname + '/dist')); 
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-app.get('/*',function(req , res ) 
-{
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-
-})
-
 
