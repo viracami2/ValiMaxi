2a2cef95c60bcf9978b0d6bd8eff39d365a27612
VictorCastro <viracami2>
2018-01-01 15:28:59 -0500

    Se añade componente de prueba donde se verifica un numero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 
+
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+
+  
+  constructor(private _http: Http) {
+    
+  }
   
   
   title = 'app Angular First!!';
@@ -25,5 +36,19 @@ export class AppComponent {
     return 'Hola mundo cruel xd'
   }
 
+
+  data: any = null;
+
+   eventoLocoUno() {
+    this.aempezar=String( this._http.get('https://apifestigaita.herokuapp.com/api/v1/bagpipes/14')
+                .map((res: Response) => res.json())
+                 .subscribe(data => {
+                        this.data = data;
+                        console.log(this.data);
+                        
+                }));
+  }
+
+  
 
 }
