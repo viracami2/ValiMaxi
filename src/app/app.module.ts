a0c4f14414cba82b2d5384b774c8e2b3adfbff88
VictorCastro <viracami2>
2017-12-31 20:59:18 -0500

    Se importa toda la data para empezar a usar angular.material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';//para animaciones
+import {MatButtonModule, MatCheckboxModule,
+        MatFormFieldModule,MatInputModule
 
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { AppCompoBasic } from './compoBasic/app.compoBasic';
+
+
 
 
 @NgModule({
@@ -14,7 +20,8 @@ import { AppCompoBasic } from './compoBasic/app.compoBasic';
     AppComponent,AppCompoBasic
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule,MatFormFieldModule,MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent] 
